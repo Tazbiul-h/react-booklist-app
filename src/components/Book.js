d07ac619711cd38c97ee652038b31d7cdc1ddfd9
Tazbiul Hasan <tazbiul.h31@gmail.com>
2021-05-12 11:36:39 +0600

    Bacis features Practice

---
+++ src/components/Book.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../stylesheets/Book.css';
+
+const Book = (props) => {
+    return(
+        <div className = "Book">
+            <h3 onClick = {props.delete}>Book: {props.bookName} </h3>
+            <h3>Writer: {props.writer} </h3>
+            <input type ="text" onChange = {props.inputName} value ={props.bookName}></input>
+
+            
+        </div>
+        
+    );
+}
+export default Book;
